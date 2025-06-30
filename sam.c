9fa813c58e958dcb05ed0f278606ec9098ab128c
lalithnextgenai <lalithnextgenai@gmail.com>
2025-06-30 13:08:53 +0530

    Circle-BRANCH

--- sam.c
+++ sam.c
@@ -4,8 +4,9 @@ void main()
 {
     printf("I am Lalith");
     int a=10,b=20;
-    float s,l,b;
+    float s,l,b,r;
     printf("The sum is",a+b);
     printf("The area of square is",s*s);
     printf("The area of rectangle is",l*b);
+     printf("The diameter of circle is",2*r);
 }
