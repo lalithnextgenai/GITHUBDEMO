2c0a436ea406a4ecf63aed8fa53204ad89dd282e
lalithnextgenai <lalithnextgenai@gmail.com>
2025-06-30 12:57:01 +0530

    Area of rectangle

--- sam.c
+++ sam.c
@@ -4,7 +4,8 @@ void main()
 {
     printf("I am Lalith");
     int a=10,b=20;
-    float s;
+    float s,l,b;
     printf("The sum is",a+b);
     printf("The area of square is",s*s);
+    printf("The area of rectangle is",l*b);
 }
